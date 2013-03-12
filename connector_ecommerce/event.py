9e3783052af00aeb17c31d759dc60e27bca75524
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2013-03-12 20:10:00 +0100

    [ADD] Base support to export picking (partial and complete)

---
+++ connector_ecommerce/event.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Joel Grand-Guillaume
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.addons.connector.event import Event
+
+    
+on_picking_done = Event()
+"""
+``on_picking_done`` is fired when a picking has been marked as done.
+
+Listeners should take the following arguments:
+
+ * session: `Session` object
+ * model_name: name of the model
+ * record_id: id of the record
+ * type: 'partial' or 'complete' depending on the picking done
+"""
