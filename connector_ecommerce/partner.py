96477d5ee6177821590ae3603d71f7d9b427e436
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-03-20 14:41:48 +0100

    [REF] move content of partner.py to old_stuff.py

--- connector_ecommerce/partner.py
+++ connector_ecommerce/partner.py
@@ -19,32 +19,3 @@
 #                                                                               #
 #################################################################################
 
-from openerp.osv import orm, fields
-
-
-class res_partner(orm.Model):
-    _inherit = 'res.partner'
-
-    _columns = {
-        'shop_ids': fields.many2many(
-            'sale.shop',
-            'sale_shop_res_partner_rel',
-            'shop_id',
-            'partner_id',
-            string='Present in Shops',
-            readonly=True,
-            help="List of shops in which this customer exists."),
-    }
-
-    # xxx move to BaseConnector _get_import_defaults_res_partner
-    def _get_default_import_values(self, cr, uid, external_session, mapping_id=None, defaults=None, context=None):
-        if external_session.sync_from_object._name == 'sale.shop':
-            shop = external_session.sync_from_object
-            if not defaults: defaults = {}
-            defaults.update({
-                'lang': shop.default_customer_lang.code,
-                'property_account_position': shop.default_fiscal_position.id,
-                'property_account_receivable': shop.default_customer_account,
-                'shop_ids': [(4, shop.id)],
-            })
-        return defaults
