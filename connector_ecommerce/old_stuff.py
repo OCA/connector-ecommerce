68e402baf0ad8a6500682ed15fb484929039c46e
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-03-20 15:01:57 +0100

    [REF] moved external_referential.py code to old_stuff.py

--- connector_ecommerce/old_stuff.py
+++ connector_ecommerce/old_stuff.py
@@ -1125,3 +1125,70 @@ class product_category(Model):
             res = (), {} # list of ids, dict of ids to date_changed
         return res
 
+
+# moved from external_referential.py #########
+
+
+from openerp.osv import orm, fields
+
+
+class external_referential(orm.Model):
+    _inherit = "external.referential"
+
+    _columns = {
+        'last_imported_product_id': fields.integer(
+            'Last Imported Product Id',
+            help="Product are imported one by one. "
+                 "This is the magento id of the last product imported. "
+                 "If you clear it all product will be imported"),
+        # TODO replace by last import date (at website level)
+        'last_imported_partner_id': fields.integer(
+            'Last Imported Partner Id',
+            help="Partners are imported one by one. "
+                 "This is the magento id of the last partner imported. "
+                 "If you clear it all partners will be imported"),
+        'import_all_attributs': fields.boolean(
+            'Import all attributs',
+            help="If the option is uncheck only the attributs "
+                 "that doesn't exist in OpenERP will be imported "),
+        'import_image_with_product': fields.boolean(
+            'With image',
+            help="If the option is check the product's image and "
+                 "the product will be imported at the same time and"
+                 "so the step '7-import images' is not needed"),
+        'import_links_with_product': fields.boolean(
+            'With links',
+            help="If the option is check the product's links "
+                 "(Up-Sell, Cross-Sell, Related) and the product will "
+                 "be imported at the same time and so the step "
+                 "'8-import links' is not needed"),
+        }
+
+    def import_customer_groups(self, cr, uid, ids, context=None):
+        self.import_resources(cr, uid, ids, 'res.partner.category', context=context)
+        return True
+
+    def import_product_categories(self, cr, uid, ids, context=None):
+        self.import_resources(cr, uid, ids, 'product.category', context=context)
+        return True
+
+    def import_customers(self, cr, uid, ids, context=None):
+        self.import_resources(cr, uid, ids, 'res.partner', context=context)
+        return True
+
+#    def import_product_attributes_sets(self, cr, uid, ids, context=None):
+#        return self.import_resources(cr, uid, ids, 'TODO', context=context)
+#
+#    def import_product_attributes_groups(self, cr, uid, ids, context=None):
+#        return self.import_resources(cr, uid, ids, 'TODO', context=context)
+#
+#    def import_product_attributes(self, cr, uid, ids, context=None):
+#        return self.import_resources(cr, uid, ids, 'TODO', context=context)
+
+    def import_products(self, cr, uid, ids, context=None):
+        self.import_resources(cr, uid, ids, 'product.product', context=context)
+        return True
+
+    def import_product_links(self, cr, uid, ids, context=None):
+        self.import_resources(cr, uid, ids, 'product.link', context=context)
+        return True
