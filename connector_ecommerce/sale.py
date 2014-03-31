944f15ff3bf948df5dbf8a8d3426fdd40e4b8e9f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-03-31 13:44:46 +0200

    The special sales orders lines can have a sequence and have low priority by default so they are likely to appear at the bottom, shipping line being the last one

--- connector_ecommerce/sale.py
+++ connector_ecommerce/sale.py
@@ -442,6 +442,7 @@ class SpecialOrderLineBuilder(ConnectorUnit):
         self.price_unit = None
         self.quantity = 1
         self.sign = 1
+        self.sequence = 90
 
     def get_line(self):
         assert self.product_ref or self.product
@@ -461,7 +462,8 @@ class SpecialOrderLineBuilder(ConnectorUnit):
                 'name': product.name,
                 'product_uom': product.uom_id.id,
                 'product_uom_qty': self.quantity,
-                'price_unit': self.price_unit * self.sign}
+                'price_unit': self.price_unit * self.sign,
+                'sequence': self.sequence}
 
 
 class ShippingLineBuilder(SpecialOrderLineBuilder):
@@ -471,6 +473,7 @@ class ShippingLineBuilder(SpecialOrderLineBuilder):
     def __init__(self, environment):
         super(ShippingLineBuilder, self).__init__(environment)
         self.product_ref = ('connector_ecommerce', 'product_product_shipping')
+        self.sequence = 99
 
 
 class CashOnDeliveryLineBuilder(SpecialOrderLineBuilder):
@@ -482,6 +485,7 @@ class CashOnDeliveryLineBuilder(SpecialOrderLineBuilder):
         super(CashOnDeliveryLineBuilder, self).__init__(environment)
         self.product_ref = ('connector_ecommerce',
                             'product_product_cash_on_delivery')
+        self.sequence = 98
 
 
 class GiftOrderLineBuilder(SpecialOrderLineBuilder):
@@ -494,6 +498,7 @@ class GiftOrderLineBuilder(SpecialOrderLineBuilder):
                             'product_product_gift')
         self.sign = -1
         self.gift_code = None
+        self.sequence = 97
 
     def get_line(self):
         line = super(GiftOrderLineBuilder, self).get_line()
