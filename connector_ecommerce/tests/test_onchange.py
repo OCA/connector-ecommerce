48ce15d53085a5e8a4f30c81be786ddf7834a406
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-07-07 11:55:58 +0200

    set modules to not installable
    Move connector_ecommerce in the root folder and set installable
    
    It now depends on sale_payment_automatic_workflow
    
    In v7. sale_automatic_workflow had a dependency on sale_payment_method.
    This is no longer true, now the module sale_payment_automatic_workflow
    makes the glue between sale_automatic_workflow and sale_payment_method.
    
    sale.shop has passed away
    
    Remove the deprecated methods used to generate special lines, yeah!
    
    stock.picking.out is no longer there
    
    Remove the fiscal position rule onchange
    
    As something similar has been implemented in v8, I'm not sure that
    the fiscal position rule will be used and if it will, the onchange signature
    will change, so let's remove it and add it again if necessary later.
    
    Adapt the onchange players to the new API
    
    Thanks to Model.onchange(), we apply the onchanges on a temporary 'new record' and get back the changed values.
    Then we apply these values on our order values.
    
    Use relative imports and remove fast_suite, checks in tests
    
    Use the new env when building special lines
    
    Rename environment to connector_env (follows connector's changes)
    
    Use the new API
    
    Correctly raise the event when a picking becomes 'done'
    
    Test that on_tracking_number_added is fired
    
    Correctly fire the event for partial pickings
    
    Update the manifest and description; bump to 3.0.0
    
    Correction of typos
    
    Use @api.one as we have nothing outside the loop
    
    Corrections of more typos

--- connector_ecommerce/tests/test_onchange.py
+++ connector_ecommerce/tests/test_onchange.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-############################################################################### 
+###############################################################################
 #
 #   connector-ecommerce for OpenERP
 #   Copyright (C) 2013-TODAY Akretion <http://www.akretion.com>.
@@ -18,9 +18,8 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-############################################################################### 
+###############################################################################
 
-import unittest2
 import mock
 
 from openerp.addons.connector_ecommerce.unit.sale_order_onchange import (
@@ -33,62 +32,80 @@ DB = common.DB
 ADMIN_USER_ID = common.ADMIN_USER_ID
 
 
-class test_onchange(common.TransactionCase):
-    """ Test if the onchanges are applied correctly on a sale order"""
+class TestOnchange(common.TransactionCase):
+    """ Test if the onchanges are applied correctly on a sales order"""
 
     def setUp(self):
-        super(test_onchange, self).setUp()
+        super(TestOnchange, self).setUp()
         self.session = ConnectorSession(self.cr, self.uid)
 
     def test_play_onchange(self):
-        """ Play the onchange ConnectorUnit on a sale order """
-        product_model = self.registry('product.product')
-        partner_model = self.registry('res.partner')
-        shop_model = self.registry('sale.shop')
-        tax_model = self.registry('account.tax')
-        cr, uid = self.cr, self.uid
+        """ Play the onchange ConnectorUnit on a sales order """
+        product_model = self.env['product.product']
+        partner_model = self.env['res.partner']
+        tax_model = self.env['account.tax']
+        payment_method_model = self.env['payment.method']
 
         backend_record = mock.Mock()
         env = Environment(backend_record, self.session, 'sale.order')
 
-        partner_id = partner_model.create(cr, uid,
-                                          {'name': 'seb',
-                                          'zip': '69100',
-                                          'city': 'Villeurbanne'})
-        partner_invoice_id = partner_model.create(cr, uid,
-                                                  {'name': 'Guewen',
-                                                   'zip': '1015',
-                                                   'city': 'Lausanne',
-                                                   'type': 'invoice',
-                                                   'parent_id': partner_id})
-        tax_id = tax_model.create(cr, uid, {'name': 'My Tax'})
-        product_id = product_model.create(cr, uid,
-                                          {'default_code': 'MyCode',
-                                           'name': 'My Product',
-                                           'weight': 15,
-                                           'taxes_id': [(6, 0, [tax_id])]})
-        shop_id = shop_model.create(cr, uid, {'name': 'My shop'})
+        partner = partner_model.create({'name': 'seb',
+                                        'zip': '69100',
+                                        'city': 'Villeurbanne'})
+        partner_invoice = partner_model.create({'name': 'Guewen',
+                                                'zip': '1015',
+                                                'city': 'Lausanne',
+                                                'type': 'invoice',
+                                                'parent_id': partner.id})
+        tax = tax_model.create({'name': 'My Tax'})
+        product = product_model.create({'default_code': 'MyCode',
+                                        'name': 'My Product',
+                                        'weight': 15,
+                                        'taxes_id': [(6, 0, [tax.id])]})
+        payment_term = self.env.ref('account.account_payment_term_advance')
+        payment_method = payment_method_model.create({
+            'name': 'Cash',
+            'payment_term_id': payment_term.id,
+        })
 
-        order_input = {
-            'shop_id': shop_id,
+        order_vals = {
             'name': 'mag_10000001',
-            'partner_id': partner_id,
+            'partner_id': partner.id,
+            'payment_method_id': payment_method.id,
             'order_line': [
-                (0, 0, {
-                    'product_id': product_id,
-                    'price_unit': 20,
-                    'name': 'My Real Name',
-                    'product_uom_qty': 1,
-                }),
-            ]
+                (0, 0, {'product_id': product.id,
+                        'price_unit': 20,
+                        'name': 'My Real Name',
+                        'product_uom_qty': 1,
+                        'sequence': 1,
+                        }
+                 ),
+            ],
+            # fake field for the lines coming from a backend
+            'backend_order_line': [
+                (0, 0, {'product_id': product.id,
+                        'price_unit': 10,
+                        'name': 'Line 2',
+                        'product_uom_qty': 2,
+                        'sequence': 2,
+                        }
+                 ),
+            ],
         }
 
-        onchange = SaleOrderOnChange(env)
-        order = onchange.play(order_input,
-                              order_input['order_line'])
+        extra_lines = order_vals['backend_order_line']
 
-        self.assertEqual(order['partner_invoice_id'], partner_invoice_id)
+        onchange = SaleOrderOnChange(env)
+        order = onchange.play(order_vals, extra_lines)
+
+        self.assertEqual(order['partner_invoice_id'], partner_invoice.id)
+        self.assertEqual(order['payment_term'], payment_term.id)
+        self.assertEqual(len(order['order_line']), 1)
         line = order['order_line'][0][2]
         self.assertEqual(line['name'], 'My Real Name')
         self.assertEqual(line['th_weight'], 15)
-        self.assertEqual(line['tax_id'][0][2][0], tax_id)
+        self.assertEqual(line['tax_id'], [(6, 0, [tax.id])])
+        line = order['backend_order_line'][0][2]
+        self.assertEqual(line['name'], 'Line 2')
+        self.assertEqual(line['th_weight'], 30)
+        self.assertEqual(line['tax_id'], [(6, 0, [tax.id])])
