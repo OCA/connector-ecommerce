b7751bf68d3acb1317be06af4bf243c8341f127d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-05-26 12:34:26 +0200

    Improve phrasing, short license headers, ...

--- connector_ecommerce/models/product.py
+++ connector_ecommerce/models/product.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Sébastien BEAU
-#    Copyright 2011-2013 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011-2013 Akretion (Sébastien Beau)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from openerp import models, fields, api
 from openerp.addons.connector.session import ConnectorSession
@@ -86,7 +69,7 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     @api.depends()
-    def _get_checkpoint(self):
+    def _compute_has_checkpoint(self):
         checkpoint_model = self.env['connector.checkpoint']
         model_model = self.env['ir.model']
         model = model_model.search([('model', '=', 'product.product')])
@@ -98,7 +81,7 @@ class ProductProduct(models.Model):
                                              )
             product.has_checkpoint = bool(points)
 
-    has_checkpoint = fields.Boolean(compute='_get_checkpoint',
+    has_checkpoint = fields.Boolean(compute='_compute_has_checkpoint',
                                     string='Has Checkpoint')
 
     @api.model
