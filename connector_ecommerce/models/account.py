3ea85b9315f2028f0dd8ca058bdc39597bc1d9e9
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-05-26 11:40:16 +0200

    Remove account.tax.group, now exists in 'account'
    Improve phrasing, short license headers, ...
    
    Rename class Environment to ConnectorEnvironment
    
    Following the upstream change

--- connector_ecommerce/models/account.py
+++ connector_ecommerce/models/account.py
@@ -1,73 +1,31 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2011-2013 Akretion
-#    @author Sébastien BEAU <sebastien.beau@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011-2013 Akretion (Sébastien Beau)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp.osv import orm, fields
+from openerp import models
 
 
-class AccountTaxCode(orm.Model):
+class AccountTaxCode(models.Model):
     _inherit = 'account.tax'
 
-    def get_tax_from_rate(self, cr, uid, rate, is_tax_included=False,
-                          context=None):
-        # TODO improve, if tax are not correctly mapped the order should
-        # be in exception (integration with sale_execption)
-        account_tax_obj = self.pool['account.tax']
-        tax_ids = account_tax_obj.search(
-            cr, uid,
+    def get_tax_from_rate(self, rate, is_tax_included=False):
+        account_tax_model = self.env['account.tax']
+        tax = account_tax_model.search(
             [('price_include', '=', is_tax_included),
              ('type_tax_use', 'in', ['sale', 'all']),
              ('amount', '>=', rate - 0.001),
-             ('amount', '<=', rate + 0.001)])
-        if tax_ids:
-            return tax_ids[0]
-        else:
-            # try to find a tax with less precision
-            tax_ids = account_tax_obj.search(
-                cr, uid,
-                [('price_include', '=', is_tax_included),
-                 ('type_tax_use', 'in', ['sale', 'all']),
-                 ('amount', '>=', rate - 0.01),
-                 ('amount', '<=', rate + 0.01)])
-            if tax_ids:
-                return tax_ids[0]
-        return False
+             ('amount', '<=', rate + 0.001)],
+            limit=1,
+        )
+        if tax:
+            return tax
 
-
-class AccountTaxGroup(orm.Model):
-    _name = 'account.tax.group'
-    _description = 'account tax group'
-
-    _columns = {
-        'name': fields.char('Name', size=64),
-        'tax_ids': fields.one2many('account.tax', 'group_id', 'Taxes'),
-    }
-
-
-class AccountTax(orm.Model):
-    _inherit = 'account.tax'
-
-    _columns = {
-        'group_id': fields.many2one(
-            'account.tax.group',
-            string='Tax Group',
-            help="Choose the tax group. "
-                 "This is needed for example with Prestashop"),
-    }
+        # try to find a tax with less precision
+        tax = account_tax_model.search(
+            [('price_include', '=', is_tax_included),
+             ('type_tax_use', 'in', ['sale', 'all']),
+             ('amount', '>=', rate - 0.01),
+             ('amount', '<=', rate + 0.01)],
+            limit=1,
+        )
+        return tax
