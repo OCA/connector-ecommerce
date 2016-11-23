f15634b5a2f0ff371a542623d64b65445b3fb9b2
sesousy <sesousy@7gates.co>
2016-11-24 01:19:29 +0200

    [MIG] Migration to 10.0

--- connector_ecommerce/wizard/sale_ignore_cancel.py
+++ connector_ecommerce/wizard/sale_ignore_cancel.py
@@ -2,7 +2,7 @@
 # © 2013-2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class SaleIgnoreCancel(models.TransientModel):
