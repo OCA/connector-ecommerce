a697c3a371ab294a137480269ec45da04d6242f0
sesousy <sesousy@7gates.co>
2016-11-24 01:19:29 +0200

    [MIG] Migration to 10.0

--- connector_ecommerce/models/account_payment_mode.py
+++ connector_ecommerce/models/account_payment_mode.py
@@ -2,7 +2,7 @@
 # © 2011-2013 Akretion (Sébastien Beau)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class AccountPaymentMode(models.Model):
