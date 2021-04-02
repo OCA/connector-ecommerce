81afd6631a4ec43e3d13320321823122c3ce60c1
Florian da Costa <florian.dacosta@akretion.com>
2021-04-02 12:15:11 +0200

    [FIX] Remove checkpoint as it has been removed from connector

--- connector_ecommerce/models/product.py
+++ connector_ecommerce/models/product.py
@@ -58,26 +58,6 @@ class ProductTemplate(models.Model):
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    @api.depends()
-    def _compute_has_checkpoint(self):
-        checkpoint_model = self.env["connector.checkpoint"]
-        model_model = self.env["ir.model"]
-        model = model_model.search([("model", "=", "product.product")])
-        for product in self:
-            points = checkpoint_model.search(
-                [
-                    ("model_id", "=", model.id),
-                    ("record_id", "=", product.id),
-                    ("state", "=", "need_review"),
-                ],
-                limit=1,
-            )
-            product.has_checkpoint = bool(points)
-
-    has_checkpoint = fields.Boolean(
-        compute="_compute_has_checkpoint", string="Has Checkpoint"
-    )
-
     @api.model
     def _price_changed_fields(self):
         return {"lst_price", "standard_price", "price", "price_extra"}
