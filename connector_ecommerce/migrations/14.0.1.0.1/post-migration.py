94a4469ca962f4914c7b86ce9d96810c9dccdb70
Stephane Mangin <stephane.mangin@camptocamp.com>
2022-05-04 11:13:16 +0200

    Missing env variable in openupgrade method call

--- connector_ecommerce/migrations/14.0.1.0.1/post-migration.py
+++ connector_ecommerce/migrations/14.0.1.0.1/post-migration.py
@@ -4,4 +4,4 @@ from openupgradelib import openupgrade
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     old_rules = ["connector_ecommerce.excep_product_has_checkpoint"]
-    openupgrade.delete_records_safely_by_xml_id(old_rules)
+    openupgrade.delete_records_safely_by_xml_id(env, old_rules)
